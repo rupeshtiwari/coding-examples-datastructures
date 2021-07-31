a46a54a7a5e680415172483ba9c72e6a43bfdd87
rupeshtiwari <roopkt@gmail.com>
2021-07-31 05:15:07 -0400

    chore(array): kadanes algo

--- src/02-Array/04-max-sub-array-sum.js
+++ src/02-Array/04-max-sub-array-sum.js
@@ -1,7 +1,23 @@
-class MaximumSubArraySum{
-    execute(array, n) {
-        
-        
-
+/**
+ *
+ * Kadane's Algorithm
+ */
+class MaximumSubArraySum {
+    /**
+     * O(n) time | O(1) space
+     * @param {number} array 
+     * @param {number} n 
+     * @returns 
+     */
+  execute(array, n) {
+    n = array.length;
+    let max = Number.MIN_SAFE_INTEGER;
+    let sum = 0;
+    for (let i = 0; i < n; i++) {
+      sum += Math.max(array[i], 0);
+      max = Math.max(max, sum);
     }
+
+    return sum;
+  }
 }
