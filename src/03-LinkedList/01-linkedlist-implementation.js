4d7414f4f2aeca57edbfed3398a3275bf08f2ba0
rupeshtiwari <roopkt@gmail.com>
2021-08-02 16:59:33 -0400

    chore: deleting 3rd element

--- src/03-LinkedList/01-linkedlist-implementation.js
+++ src/03-LinkedList/01-linkedlist-implementation.js
@@ -10,6 +10,14 @@ class LinkedList {
     let currentNode = head;
     let currentPosition = 1;
 
+    if (position === 1) {
+      // insert at the begining
+      newNode.next = head;
+      head = newNode;
+
+      return head;
+    }
+
     while (currentNode.next && currentPosition < position) {
       currentNode = currentNode.next;
       currentPosition++;
@@ -19,6 +27,41 @@ class LinkedList {
       let temp = currentNode.next;
       currentNode.next = newNode;
       newNode.next = temp;
+    } else {
+      currentNode.next = newNode;
+    }
+
+    return head;
+  }
+
+  delete(head, position = 0) {
+    if (head == null) {
+      return head;
+    }
+
+    let currentNode = head;
+    let currentPosition = 1;
+
+    if (position === 1) {
+      // Delete at the begining
+      let currentHead = head;
+      head = head.next;
+      currentHead = null;
+
+      return head;
+    }
+
+    let previous = null;
+
+    while (currentNode.next && currentPosition < position) {
+      previous = currentNode;
+      currentNode = currentNode.next;
+      currentPosition++;
+    }
+
+    if (previous && previous.next) {
+      previous.next = currentNode.next;
+      currentNode = null;
     } else {
       currentNode.next = newNode;
     }
@@ -47,32 +90,60 @@ class LinkListNode {
   }
 }
 
-describe('@LinkedList : Insert', () => {
-  it('Problem #1', () => {
-    var start = performance.now();
-    let linkedList = new LinkedList();
-    let head = null;
+describe('@LinkedList', () => {
+  describe('Can Insert', () => {
+    it('Problem #1', () => {
+      var start = performance.now();
+      let linkedList = new LinkedList();
+      let head = null;
+      head = linkedList.insert(head, 2);
+      head = linkedList.insert(head, 1, 1);
 
-    [1, 2, 4, 5].forEach((e, idx) => {
-      head = linkedList.insert(head, e, idx);
+      expect(linkedList.read(head)).toEqual([1, 2]);
+      var end = performance.now();
+      console.log('Problem #1', end - start);
     });
 
-    expect(linkedList.read(head)).toEqual([1, 2, 4, 5]);
-    var end = performance.now();
-    console.log('Problem #1', end - start);
+    it('Problem #2', () => {
+      var start = performance.now();
+      let linkedList = new LinkedList();
+      let head = null;
+      head = linkedList.insert(head, 2, 0);
+      head = linkedList.insert(head, 1, 1);
+      head = linkedList.insert(head, 3, 2);
+
+      expect(linkedList.read(head)).toEqual([1, 2, 3]);
+      var end = performance.now();
+      console.log('Problem #2', end - start);
+    });
   });
+  describe('Can Delete', () => {
+    it('Problem #1', () => {
+      var start = performance.now();
+      let linkedList = new LinkedList();
+      let head = null;
+      head = linkedList.insert(head, 2);
+      head = linkedList.insert(head, 1, 1);
+      head = linkedList.insert(head, 3, 2);
+      head = linkedList.delete(head, 1);
 
-  it('Problem #2', () => {
-    var start = performance.now();
-    let linkedList = new LinkedList();
-    let head = null;
-
-    [1, 2, 4, 5].forEach((e, idx) => {
-      head = linkedList.insert(head, e, idx);
+      expect(linkedList.read(head)).toEqual([2, 3]);
+      var end = performance.now();
+      console.log('Problem #1', end - start);
     });
-    linkedList.insert(head, 3, 2);
-    expect(linkedList.read(head)).toEqual([1, 2, 3, 4, 5]);
-    var end = performance.now();
-    console.log('Problem #2', end - start);
+    it('Problem #2', () => {
+      var start = performance.now();
+      let linkedList = new LinkedList();
+      let head = null;
+      head = linkedList.insert(head, 2);
+      head = linkedList.insert(head, 1, 1);
+      head = linkedList.insert(head, 3, 2);
+      // [1,2,3]
+      head = linkedList.delete(head, 3); // Delete 3 element
+
+      expect(linkedList.read(head)).toEqual([1, 2]);
+      var end = performance.now();
+      console.log('Problem #2', end - start);
+    });
   });
 });
